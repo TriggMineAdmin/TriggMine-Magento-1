c99d2b7ad41e8976bd21d63f006d2b55c129551c
TriggMine <admin@triggmine.com>
2017-04-07 16:35:51 +0300

    deploy v. 3.0.13.2

---
+++ app/code/community/Triggmine/IntegrationModule/Model/Observer.php
@@ -0,0 +1,96 @@
+<?php
+
+class Triggmine_IntegrationModule_Model_Observer
+{
+    public function send_page_init(Varien_Event_Observer $observer)
+    {   
+        if (Mage::helper('integrationmodule/data')->isEnabled())
+        {
+            $data = Mage::helper('integrationmodule/data')->PageInit($observer);
+            Mage::helper('integrationmodule/data')->onPageInit($data);
+        }
+    }    
+    
+    public function diagnostic_information_updated(Varien_Event_Observer $observer)
+    {   
+        $data = Mage::helper('integrationmodule/data')->SoftChek($observer);
+        $res = Mage::helper('integrationmodule/data')->onDiagnosticInformationUpdated($data);
+        
+        if ($res["status"] === 503)
+        {   
+            Mage::throwException(Mage::helper('adminhtml')->__('Invalid API URL'));
+        }
+        else if ($res["status"] === 401)
+        {   
+            Mage::throwException(Mage::helper('adminhtml')->__('Invalid API KEY'));
+        }
+    }
+    
+    public function export_order_history(Varien_Event_Observer $observer)
+    {
+        if (Mage::helper('integrationmodule/data')->isEnabled() &&
+            Mage::helper('integrationmodule/data')->exportOrderEnabled())
+        {
+            $data = Mage::helper('integrationmodule/data')->getOrderHistory($observer);
+            Mage::helper('integrationmodule/data')->exportOrderHistory($data);
+        }
+    }
+    
+    public function export_customer_history(Varien_Event_Observer $observer)
+    {
+        if (Mage::helper('integrationmodule/data')->isEnabled() &&
+            Mage::helper('integrationmodule/data')->exportCustomerEnabled())
+        {
+            $data = Mage::helper('integrationmodule/data')->getCustomerHistory($observer);
+            Mage::log(json_encode($data), null, 'export.log');
+            //Mage::helper('integrationmodule/data')->exportCustomerHistory($data);
+        }
+    }
+
+    public function SalesOrderPlaceAfter(Varien_Event_Observer $observer)
+    {
+        if (Mage::helper('integrationmodule/data')->isEnabled())
+        {
+            $data = Mage::helper('integrationmodule/data')->getOrderData($observer);
+            Mage::helper('integrationmodule/data')->onConvertCartToOrder($data);
+        }
+    }
+
+    public function CheckoutCartSaveAfter(Varien_Event_Observer $observer)
+    {
+        if (Mage::helper('integrationmodule/data')->isEnabled())
+        {
+            $data = Mage::helper('integrationmodule/data')->getCartData();
+            Mage::helper('integrationmodule/data')->sendCart($data);
+        }
+    }
+
+    public function CustomerRegisterSuccess(Varien_Event_Observer $observer)
+    {
+        if (Mage::helper('integrationmodule/data')->isEnabled())
+        {
+            $event = $observer->getEvent();
+            
+            $data = Mage::helper('integrationmodule/data')->getCustomerRegisterData($event);
+            Mage::helper('integrationmodule/data')->sendRegisterData($data);
+        }
+    }
+
+    public function CustomerLogin(Varien_Event_Observer $observer)
+    {
+        if (Mage::helper('integrationmodule/data')->isEnabled())
+        {
+            $data = Mage::helper('integrationmodule/data')->getCustomerLoginData();
+            Mage::helper('integrationmodule/data')->sendLoginData($data);
+        }
+    }
+
+    public function CustomerLogout(Varien_Event_Observer $observer)
+    {
+        if (Mage::helper('integrationmodule/data')->isEnabled())
+        {
+            $data = Mage::helper('integrationmodule/data')->getCustomerLoginData();
+            Mage::helper('integrationmodule/data')->sendLogoutData($data);
+        }
+    }
+}
